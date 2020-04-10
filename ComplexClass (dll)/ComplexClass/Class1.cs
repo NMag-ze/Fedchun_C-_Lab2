6d5068da12e004e76b6d74190274bc669afe36e4
NMag-ze <n.magnitowsky@gmail.com>
2020-04-11 02:34:06 +0300

    Add project

---
+++ ComplexClass (dll)/ComplexClass/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexClass
+{
+    //dll-библиотека для работы с комплексными числами
+    public class Complex
+    {
+        double re, im;
+        public Complex(double r,double i)
+        {
+            re = r;
+            im = i;
+        }
+
+        public static Complex sum(Complex C1, Complex C2)
+        {
+            return new Complex(C1.re + C2.re, C1.im + C2.im);
+        }
+        public override string ToString()
+        {
+            return $" {re} + {im} ";
+        }
+
+    }
+}
---
+++ TestComplexClass/TestComlex/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ComplexClass;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestComlex
+{
+    class Program
+    {
+        //LabWork2\ComplexClass\ComplexClass\bin\Debug\ComplexClass.dll
+        static void Main(string[] args)
+        {
+            Complex t1 = new Complex(1.0, 1.0);
+            Complex t2 = new Complex(2.0, 3.0), t3;
+            t3 = Complex.sum(t1, t2);
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+
+        }
+    }
+}
